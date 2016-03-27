ce48d8a7e7ae087f27136a0fd93497b751ce26fb
Tim Martin <tim@asymptotic.co.uk>
2016-03-27 18:36:54 +0100

    Ferns can be randomly regenerated

--- fern.js
+++ fern.js
@@ -1,6 +1,6 @@
 if ( ! Detector.webgl ) Detector.addGetWebGLMessage();
 
-var camera, scene, renderer, material;
+var camera, scene, renderer, material, fernObject;
 
 // We use a fixed aspect ratio. This makes it simple to scale the
 // canvas to the width of its containing element without having to
@@ -10,11 +10,18 @@ var aspectRatio = 1.6;
 $(function() {
     init();
     animate();
+
+    $("#regenerate_button").click(regenerate_fern);
 });
 
 var fernState = {
     stemLength: 0
 };
+
+function regenerate_fern() {
+    scene.remove(fernObject);
+    generate_fern();
+}
 
 function rotateX(amount) {
     return function(geometry, fernState) {
@@ -57,14 +64,18 @@ function leaf(stemDistance, tipOffset) {
     }
 }
 
-var fernActions = [
-    rotateX(0.1),
-    rotateZ(0.01),
-    scale(0.8),
-    stem(30),
-    leaf(1, new THREE.Vector3(50, 10, 0)),
-    leaf(1, new THREE.Vector3(-40, 20, 0))
-];
+function generateFernActions() {
+    return [
+        rotateX(Math.random()),
+        rotateZ(Math.random()),
+        scale(0.5 + Math.random() * 0.4),
+        stem(30),
+        leaf(1, new THREE.Vector3(50, 10, 0)),
+        leaf(1, new THREE.Vector3(-40, 20, 0))
+    ];
+}
+
+var fernActions;
 
 function fern(level) {
     if (level <= 0) {
@@ -80,6 +91,23 @@ function fern(level) {
     }
 
     return geometry;
+}
+
+function generate_fern() {
+    fernActions = generateFernActions();
+    
+    var geometry = fern(15);
+    geometry.normalize();
+    geometry.scale(100, 100, 100);
+    geometry.translate(0, -40, -15);
+
+    material = new THREE.LineBasicMaterial({color: 0xffffff,
+                                            opacity: 1,
+                                            linewidth: 3,
+                                            vertexColors: THREE.NoColors});
+
+    fernObject = new THREE.LineSegments(geometry, material);
+    scene.add(fernObject);
 }
 
 function init() {
@@ -99,20 +127,9 @@ function init() {
 
     $("#fern_container").append(renderer.domElement);
 
-    var geometry = fern(12);
-    geometry.normalize();
-    geometry.scale(100, 100, 100);
-    geometry.translate(0, -40, -15);
-
-    material = new THREE.LineBasicMaterial({color: 0xffffff,
-                                            opacity: 1,
-                                            linewidth: 3,
-                                            vertexColors: THREE.NoColors});
-
-    var line = new THREE.LineSegments(geometry, material);
-    scene.add( line );
-
     window.addEventListener('resize', onWindowResize, false);
+
+    generate_fern();
 }
 
 function onWindowResize() {
