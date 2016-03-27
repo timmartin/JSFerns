7eec1f14ce9f56c7ee536724000f2ada1688f443
Tim Martin <tim@asymptotic.co.uk>
2016-03-27 17:57:54 +0100

    Abstracted drawing steps into a series of actions
    This is the first step towards making it possible to generate ferns
    automatically.

--- fern.js
+++ fern.js
@@ -1,14 +1,75 @@
 if ( ! Detector.webgl ) Detector.addGetWebGLMessage();
 
-var windowHalfX = window.innerWidth / 2,
-    windowHalfY = window.innerHeight / 2,
-
-    camera, scene, renderer, material;
+var camera, scene, renderer, material;
 
 $(function() {
     init();
     animate();
 });
+
+var fernSettings = {
+    stem: function (geometry) {
+	var end = new THREE.Vector3(0, 30, 10);
+	geometry.translate(0, 30, 10);
+	geometry.vertices.push(new THREE.Vector3(0, 0, 0));
+	geometry.vertices.push(end);
+	return end;
+    }
+};
+
+var fernState = {
+    stemLength: 0
+};
+
+function rotateX(amount) {
+    return function(geometry, fernState) {
+	geometry.rotateX(amount);
+    }
+}
+
+function rotateZ(amount) {
+    return function(geometry, fernState) {
+	geometry.rotateZ(amount);
+    }
+}
+
+function scale(amount) {
+    return function(geometry) {
+	geometry.scale(amount, amount, amount);
+    }
+}
+
+function stem(length) {
+    return function(geometry, fernState) {
+        geometry.translate(0, length, 0);
+	geometry.vertices.push(new THREE.Vector3(0, 0, 0));
+	geometry.vertices.push(new THREE.Vector3(0, length, 0));
+	fernState.stemLength = length;
+    }
+}
+
+function leaf(stemDistance, tipOffset) {
+    return function(geometry, fernState) {
+        var startPoint =
+            new THREE.Vector3(0,
+                              stemDistance * fernState.stemLength,
+                              0);
+        geometry.vertices.push(startPoint);
+
+        var endPoint = startPoint.clone();
+        endPoint.add(tipOffset);
+        geometry.vertices.push(endPoint);
+    }
+}
+
+var fernActions = [
+    rotateX(0.1),
+    rotateZ(0.01),
+    scale(0.8),
+    stem(30),
+    leaf(1, new THREE.Vector3(50, 10, 0)),
+    leaf(1, new THREE.Vector3(-40, 20, 0))
+];
 
 function fern(level) {
     if (level <= 0) {
@@ -18,17 +79,10 @@ function fern(level) {
 	geometry = fern(level - 1);
     }
 
-    geometry.rotateX(0.1);
-    geometry.rotateZ(0.01);
-    geometry.scale(0.8, 0.8, 0.8);
-    geometry.translate(0, 30, 10);
-
-    geometry.vertices.push(new THREE.Vector3(0, 0, 0));
-    geometry.vertices.push(new THREE.Vector3(0, 30, 10));
-    geometry.vertices.push(new THREE.Vector3(0, 30, 10));
-    geometry.vertices.push(new THREE.Vector3(50, 40, 0));
-    geometry.vertices.push(new THREE.Vector3(0, 30, 10));
-    geometry.vertices.push(new THREE.Vector3(-40, 70, 0));
+    var i;
+    for (i = 0; i < fernActions.length; i++) {
+	fernActions[i](geometry, fernState)
+    }
 
     return geometry;
 }
@@ -48,9 +102,9 @@ function init() {
 
     scene = new THREE.Scene();
 
-    renderer = new THREE.WebGLRenderer( { antialias: true } );
-    renderer.setPixelRatio( window.devicePixelRatio );
-    renderer.setSize( window.innerWidth, window.innerHeight );
+    renderer = new THREE.WebGLRenderer({antialias: true});
+    renderer.setPixelRatio(window.devicePixelRatio);
+    renderer.setSize(window.innerWidth, window.innerHeight);
 
     container.appendChild( renderer.domElement );
 
@@ -73,9 +127,6 @@ function init() {
 }
 
 function onWindowResize() {
-    windowHalfX = window.innerWidth / 2;
-    windowHalfY = window.innerHeight / 2;
-
     camera.aspect = window.innerWidth / window.innerHeight;
     camera.updateProjectionMatrix();
 
@@ -92,9 +143,9 @@ function render() {
 
     var time = Date.now() * 0.0005;
 
-    for ( var i = 0; i < scene.children.length; i ++ ) {
+    for (var i = 0; i < scene.children.length; i++) {
 	var object = scene.children[ i ];
-	if ( object instanceof THREE.Line ) {
+	if (object instanceof THREE.Line) {
 	    object.rotation.y = time;
 	}
     }
